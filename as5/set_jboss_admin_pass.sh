c27b8bd1f4e34b3334f0bfa4510d7029ac425268
tifayuki <tifayuki@gmail.com>
2014-09-05 16:27:37 -0400

    repo structure refocator

---
+++ as5/set_jboss_admin_pass.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+
+if [ -f /.jboss_admin_pass_configured ]; then
+    echo "JBoss admin user's password has been configured!"
+    exit 0
+fi
+
+if [ "$JBOSS_PASS" = "**Random**" ]; then
+    unset JBOSS_PASS
+fi
+
+
+#generate password
+PASS=${JBOSS_PASS:-$(pwgen -s 12 1)}
+_word=$( [ ${JBOSS_PASS} ] && echo "preset" || echo "random" )
+
+echo "=> Configuring admin user with a ${_word} password in JBoss"
+echo "admin:${PASS}" > /jboss-5.1.0.GA/server/default/conf/props/jmx-console-users.properties
+echo "=> Done!"
+echo "========================================================================"
+echo "You can now configure to this JBoss server using:"
+echo ""
+echo "    admin:${PASS}"
+echo ""
+echo "========================================================================"
+
+touch /.jboss_admin_pass_configured
